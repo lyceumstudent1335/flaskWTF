d2aec9de72c98ada790bf5ee9f070d53a3a171cb
lyceumstudent1335 <dawidsuan@yandex.ru>
2025-03-06 19:31:27 +0300

    The page with trainers has been created

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 
+USERNAME = "David Suanov"
 app = Flask(__name__)
 
 
 @app.route("/<string:title>")
 @app.route("/index/<string:title>")
-def custom_titled_index(title: str):
-    return render_template("index.html", title=title, username="David Suanov")
+def custom_titled_index(title: str) -> str:
+    return render_template("index.html", title=title, username=USERNAME)
+
+
+@app.route("/training/<string:prof>")
+def training(prof: str) -> str:
+    return render_template("training.html", title="Тренажеры", username=USERNAME, prof=prof.lower())
 
 
 if __name__ == "__main__":
